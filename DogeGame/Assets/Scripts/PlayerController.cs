5a6e95cb6fbd252f364b4b4cef410f022773d2e7
Blaskec <59925899+Blaskec@users.noreply.github.com>
2021-07-11 11:26:16 +0200

    added SHIFT run, fixed some animations bugs
    - added shift RUN which increases speed from 5 to 8
    - fixed many bugs with animations
    
    - there are few more bugs with jump animations eh

--- DogeGame/Assets/Scripts/PlayerController.cs
+++ DogeGame/Assets/Scripts/PlayerController.cs
@@ -26,14 +26,31 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Animator jump and isGrounded booleans
         animator.SetBool("Jump", !controller.isGrounded);
         animator.SetBool("isGrounded", controller.isGrounded);
+
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         //moveDirection = new Vector3(horizontalInput, 0f, verticalInput);
         moveDirection = controller.transform.forward * verticalInput;
         animator.SetFloat("Speed", moveDirection.magnitude);
+        if (moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
+        {
+            //move
+            moveSpeed = 5f;
+            animator.SetBool("isRunning", false);
+        }
+        else if(moveDirection == Vector3.zero)
+        {
+            animator.SetBool("isRunning", false);
+        }
+        else
+        {
+            moveSpeed = 8f;
+            animator.SetBool("isRunning", true);
+        }
 
         if (controller.isGrounded)
         {
@@ -46,12 +63,16 @@ public class PlayerController : MonoBehaviour
                 
             }
         }
+        //Gravity
         directionY -= gravityScale * Time.deltaTime;
         moveDirection.y = directionY;
 
+        //Rotation
         controller.transform.Rotate(Vector3.up * horizontalInput * (175f * Time.deltaTime));
 
+        //reset Grounded for animations
         animator.SetBool("isGrounded", controller.isGrounded);
+        //actual movement with .Move function
         controller.Move(moveDirection * moveSpeed * Time.deltaTime);
         
     }
